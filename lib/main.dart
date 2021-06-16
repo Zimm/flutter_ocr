53b5a15764e9a2d0e3c77a35364cdbfd8d79c14e
= <=>
2021-06-16 17:05:15 +0300

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import './splash_screen.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      theme: ThemeData.dark(),
+      home: Splash(),
+      debugShowCheckedModeBanner: true,
+    );
+  }
+}
